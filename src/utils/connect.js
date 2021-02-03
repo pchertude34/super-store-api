f87803336ec9025f9fb95336cf26dbe10bbd884b
Payton Chertude <p.chertude34@gmail.com>
2021-02-02 19:00:12 -0800

    target the actual database

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -4,7 +4,7 @@ const { DB_USERNAME, DB_PASSWORD } = process.env;
 
 function connect() {
   return mongoose.connect(
-    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tdlyc.mongodb.net/test`,
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tdlyc.mongodb.net/super-store`,
     { useNewUrlParser: true }
   );
 }
