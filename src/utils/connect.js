39566e478f629470fa770138fe1dc42d13e9cf34
Payton Chertude <p.chertude34@gmail.com>
2021-02-02 18:46:03 -0800

    remove test logs

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 
 const { DB_USERNAME, DB_PASSWORD } = process.env;
 
-console.log('DB_USERNAME', DB_USERNAME);
-console.log('DB_PASSWORD', DB_PASSWORD);
-
 function connect() {
   return mongoose.connect(
     `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tdlyc.mongodb.net/test`,
